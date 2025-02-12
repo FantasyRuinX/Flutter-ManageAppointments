4ffb170125a813692162e5ac5b86b6f4e091c7b7
sbayat023 <samuelbayat707@gmail.com>
2025-02-13 00:14:36 +0200

    Updated UI for addAppointment

--- lib/UI/addAppointment.dart
+++ lib/UI/addAppointment.dart
@@ -15,20 +15,34 @@ class AddAppointment extends StatelessWidget {
       ),
       body: const Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          spacing: 10,
           children: <Widget>[
-            TextField(
+            SizedBox(height: 5),
+            Text("Please enter all of the following information"),
+            SizedBox(
+                height: 30, width : 300,
+                child : TextField(
                 decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0,),
                     border: OutlineInputBorder(),
-                    hintText: "Enter client name")),
+                    hintText: "Enter client name"))),
             TextField(
                 decoration: InputDecoration(
                     border: OutlineInputBorder(), hintText: "Enter time")),
-            TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Enter location")),
-            TextField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(), hintText: "Enter payment")),
+            SizedBox(
+                height: 30, width : 300,
+                child : TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0,),
+                        border: OutlineInputBorder(),
+                        hintText: "Enter location"))),
+            SizedBox(
+                height: 30, width : 300,
+                child : TextField(
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 50.0,),
+                        border: OutlineInputBorder(),
+                        hintText: "Enter payment amount"))),
             TextField(
                 decoration: InputDecoration(
                     border: OutlineInputBorder(),
