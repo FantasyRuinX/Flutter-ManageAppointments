9e311f72264dcbe792b2c6d0ebe51e86267f09e8
sbayat023 <samuelbayat707@gmail.com>
2025-02-22 20:53:21 +0200

    Updated showDialog UI and also add a indicator on the date where a appointment is set

--- lib/UI/home.dart
+++ lib/UI/home.dart
@@ -17,8 +17,9 @@ class _MyHomePageState extends State<MyHomePage> {
   DateTime? _selectedDate;
   DateTime _focusedDate = DateTime.now();
   int _selectedItem = 0;
-  List<Map<String,Event>> clients = [];
+  List<Map<String, Event>> clients = [];
   List<String> clientNames = [];
+  List<String> clientDates = [];
 
   @override
   void initState() {
@@ -33,16 +34,21 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   Future<void> loadData() async {
-    final eventViewModel = Provider.of<EventViewModel>(context,listen: false);
+    final eventViewModel = Provider.of<EventViewModel>(context, listen: false);
     await eventViewModel.readDB();
     await eventViewModel.getClientNames();
     setState(() {
       clients = eventViewModel.organisedEvents;
       clientNames = eventViewModel.clientNames;
+
+      for (Map<String, Event> event in clients){
+        clientDates.add((event.values.first).date.split("T")[0]);
+      }
+
     });
   }
 
-  void setCurrentItem(BuildContext context, int index) {
+  void setCurrentItem(BuildContext context, EventViewModel eventViewModel, int index) {
     //Show Dialog variables
     setState(() {
       _selectedItem = index;
@@ -55,31 +61,81 @@ class _MyHomePageState extends State<MyHomePage> {
           Navigator.pushNamed(context, "/listAppointments");
           break;
         case 2:
-          Navigator.pushNamed(context, "/changeAppointments");
+          eventViewModel.clearDB();
+          showDialog(
+              context: context,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: const Text("All data cleared"),
+                  actions: [
+                    TextButton(
+                        onPressed: () {
+                          Navigator.of(context).pop();
+                        },
+                        child: const Text("Close")),
+                  ],
+                );
+              });
           break;
       }
     });
   }
 
-  String setOutputEventText(Map<String,Event> event){
-    return "${event.keys.first} : ${(event.values.first).date}\n${(event.values.first).info}";
+  String setOutputEventText(Map<String, Event> event) {
+    List<String> eventTime = (event.values.first).date.split("T");
+    return "${event.keys.first} at ${eventTime[1]}";
   }
 
   Widget appointmentList() {
+    List<Map<String, Event>> clientsOnDay = [];
+    for (Map<String, Event> event in clients){
+      if ((event.values.first).date.split("T")[0] == _focusedDate.toString().split(" ")[0]){
+        clientsOnDay.add(event);
+      }
+    }
+
     return ListView.builder(
       padding: const EdgeInsets.all(8),
-      itemCount: clients.length,
+      itemCount: clientsOnDay.length,
       itemBuilder: (BuildContext context, int index) {
         return Row(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
           spacing: 10,
           children: <Widget>[
-            FloatingActionButton.small(
+            TextButton(
+                style: ButtonStyle(
+                    backgroundColor: WidgetStateProperty.all(
+                        Theme.of(context).colorScheme.inversePrimary)),
                 onPressed: () => setState(() {
-                  clients.removeAt(index);
+                      showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            return AlertDialog(
+                              title: Text(clientsOnDay[index].keys.first),
+                              content: Text(
+                                  "Location : ${(clientsOnDay[index].values.first).location}\nPayment : R${(clients[index].values.first).rand}\n${(clients[index].values.first).info}"),
+                              buttonPadding: const EdgeInsets.all(20),
+                              actions: [
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text("Close")),
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text("Remove")),
+                                TextButton(
+                                    onPressed: () {
+                                      Navigator.of(context).pop();
+                                    },
+                                    child: const Text("Change")),
+                              ],
+                            );
+                          });
                     }),
-                child: const Icon(Icons.clear)),
-            Text(setOutputEventText(clients[index])),
+                child: Text(setOutputEventText(clientsOnDay[index])))
           ],
         );
       },
@@ -108,8 +164,9 @@ class _MyHomePageState extends State<MyHomePage> {
 
                 onDaySelected: (selectedDay, focusedDay) async {
                   if (!isSameDay(_selectedDate, selectedDay)) {
-                    await eventViewModel.readDB();
+                    print(selectedDay);
 
+                    await eventViewModel.readDB();
                     await eventViewModel.getClientNames();
                     setState(() {
                       _selectedDate = selectedDay;
@@ -121,14 +178,9 @@ class _MyHomePageState extends State<MyHomePage> {
                 },
                 //----------------
 
-                eventLoader: (day) => [1, 2].isNotEmpty ? [1] : [],
+                eventLoader: (day) => clientDates.contains(day.toString().split(" ")[0]) ? [1] : [],
               ),
               const SizedBox(height: 10),
-              ElevatedButton(
-                  onPressed: () async {
-                    eventViewModel.clearDB();
-                  },
-                  child: Icon(Icons.remove)),
               Expanded(child: appointmentList()),
               const SizedBox(height: 10),
             ],
@@ -142,7 +194,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   icon: Icon(Icons.remove), label: "Clear database"),
             ],
             currentIndex: _selectedItem,
-            onTap: (i) => setCurrentItem(context, i),
+            onTap: (i) => setCurrentItem(context, eventViewModel, i),
             type: BottomNavigationBarType.fixed,
             //Show all 4 icons because more than 3 makes it invisible
             selectedItemColor: Colors.indigo[900],
