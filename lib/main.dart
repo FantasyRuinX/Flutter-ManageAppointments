f3e185ac65d034a0788b5f7b3cdbb369fd795ca5
sbayat023 <samuelbayat707@gmail.com>
2025-03-05 19:32:39 +0200

    Added transition animations

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 import 'package:Flutter_ManageAppointments/Data/eventViewModel.dart';
 
@@ -7,11 +8,10 @@ import 'UI/addAppointment.dart';
 import 'UI/listAppointment.dart';
 
 void main() {
-  runApp(
-    MultiProvider(providers: [
-      ChangeNotifierProvider(create: (_) => EventViewModel())],
-      child: const MyApp(),
-    ));
+  runApp(MultiProvider(
+    providers: [ChangeNotifierProvider(create: (_) => EventViewModel())],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
@@ -20,19 +20,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      initialRoute: "/home",
-      routes: {
-        "/home" : (context) => const MyHomePage(title: 'Appointments'),
-        "/addAppointments" : (context) => const AddAppointment(title: 'Manage appointments'),
-        "/listAppointments" : (context) => const ListAppointment(title: 'List appointments'),
-      },
-
+      initialRoute: '/home',
+      getPages: [
+        GetPage(
+            name: '/home',
+            page: () => const MyHomePage(title: 'Appointments'),
+            transition: Transition.downToUp,
+            transitionDuration: const Duration(seconds: 2),
+            curve: Curves.easeInOut),
+        GetPage(
+            name: '/addAppointments',
+            page: () => const AddAppointment(title: 'Manage appointments'),
+            transition: Transition.downToUp,
+            transitionDuration: const Duration(seconds: 2),
+            curve: Curves.easeInOut),
+        GetPage(
+            name: '/listAppointments',
+            page: () => const ListAppointment(title: 'List appointments'),
+            transition: Transition.downToUp,
+            transitionDuration: const Duration(seconds: 2),
+            curve: Curves.easeInOut),
+      ],
     );
   }
 }
